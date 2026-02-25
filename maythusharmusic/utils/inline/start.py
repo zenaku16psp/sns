30ead29a2d29f718ab07346bfdd407c410e4a689
zenaku16psp <pyaesone5p.s.p@gmail.com>
2026-02-25 17:33:55 +0630

    Update start.py

--- maythusharmusic/utils/inline/start.py
+++ maythusharmusic/utils/inline/start.py
@@ -20,20 +20,18 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["S_B_1"], 
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users"
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style="success"),
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style="success"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
-        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
