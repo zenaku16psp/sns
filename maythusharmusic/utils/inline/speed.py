0d0d80bfb9bf3e5a5d3e33f559aada449cddb1f5
zenaku16psp <pyaesone5p.s.p@gmail.com>
2026-02-28 23:18:45 +0630

    Update speed.py

--- maythusharmusic/utils/inline/speed.py
+++ maythusharmusic/utils/inline/speed.py
@@ -1,41 +1,39 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def speed_markup(_, chat_id):
-    upl = InlineKeyboardMarkup(
+    buttons = [
         [
-            [
-                InlineKeyboardButton(
-                    text="🕒 0.5x",
-                    callback_data=f"SpeedUP {chat_id}|0.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕓 0.75x",
-                    callback_data=f"SpeedUP {chat_id}|0.75",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["P_B_4"],
-                    callback_data=f"SpeedUP {chat_id}|1.0",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🕤 1.5x",
-                    callback_data=f"SpeedUP {chat_id}|1.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕛 2.0x",
-                    callback_data=f"SpeedUP {chat_id}|2.0",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
-                ),
-            ],
-        ]
-    )
-    return upl
+            InlineKeyboardButton(
+                text="🕒 0.5x",
+                callback_data=f"SpeedUP {chat_id}|0.5",
+            ),
+            InlineKeyboardButton(
+                text="🕓 0.75x",
+                callback_data=f"SpeedUP {chat_id}|0.75",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["P_B_4"],
+                callback_data=f"SpeedUP {chat_id}|1.0",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🕤 1.5x",
+                callback_data=f"SpeedUP {chat_id}|1.5",
+            ),
+            InlineKeyboardButton(
+                text="🕛 2.0x",
+                callback_data=f"SpeedUP {chat_id}|2.0",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+            ),
+        ],
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
